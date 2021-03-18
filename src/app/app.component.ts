e4fe0715e6d26feb0ce467baa00266e714106ab6
Gustavo <guz.verdesoto@gmail.com>
2021-03-18 17:23:29 -0500

    ciclo while

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,21 +9,45 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AppComponent {
   title = 'CAIZA-VERDESOTO';
 
+  @Input() valor1?:string;
+  @Input() valor2?:string;
+  proceso=<HTMLInputElement>document.getElementById("process");
+
+  impresion= "";
+
   // constructor(){}
   sumar(){
-    @Input() valor1?:string;
-    @Input() valor2?:string;
-    // let valor1="";
+    let resultado=<HTMLInputElement>document.getElementById("txtresultado");
     // let valor2="";
     let digitosTomados=-1;
-    let subvalor1:number;
-    let subvalor2:number;
+    let subvalor1:string;
+    let subvalor2:string;
     let subTotal1:number;
     let subTotal2:number;
     let auxsubTotal1;
     let auxsubTotal2;
+    let aux;
+    aux=Number(this.valor2).toString().length;
+    aux=aux*-1;
+    console.log(aux);
+    // while (digitosTomados>=aux) {
+      subvalor1=Number(this.valor1).toString().substr(digitosTomados,1);
+      subvalor2=Number(this.valor2).toString().substr(digitosTomados,1);
+      digitosTomados++;
+      subTotal1=(Number(subvalor1)+ Number(subvalor2));
+      if (subTotal1>9) {
+        auxsubTotal1=subTotal1.toString().substr(-2,1)
+      }else{
+        auxsubTotal1=0;
+      }
 
-    subvalor1=Number()
+      this.impresion += subvalor1 + " + " + subvalor2 + " = " + auxsubTotal1;
+      console.log(auxsubTotal1);
+      console.log(this.impresion);
+    // }
+    this.proceso.value = this.impresion;
+
   }
 }
+
 
